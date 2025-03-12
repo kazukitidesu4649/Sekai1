12ff23a2291d4c146a787c59444752e34ebabfdb
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-03-12 21:53:20 +0900

    topページ作成　アクセス完了

---
+++ resources/views/top.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center mt-5">
+    <h1 class="mb-4">世界に一つだけの Love Song</h1>
+    <p>二人だけのオリジナルソングを作りませんか？</p>
+    
+    <div class="row mt-4">
+        <div class="col-md-6 mx-auto">
+            <div class="card shadow-lg p-4">
+                <h3>オリジナルソングの魅力</h3>
+                <ul class="list-unstyled">
+                    <li>✨ 入退場のBGM</li>
+                    <li>🎥 プロフィールムービーのBGM</li>
+                    <li>🎤 余興でパフォーマンス</li>
+                    <li>🎁 サプライズギフト</li>
+                </ul>
+                <p class="mt-3">あなたの特別な想いを形にする楽曲制作サービスです。</p>
+                <a href="#" class="btn btn-primary">詳しく見る</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/top', function() {
+    return view('top');
+});
