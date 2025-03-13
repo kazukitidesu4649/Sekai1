6fe40e65c84f79613ed46b703f303afb27cc7e0d
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-03-14 03:38:36 +0900

    コンタクトテーブル作成 カラム[name, email, phon, message]を追加

--- database/migrations/2025_03_14_033503_create_contacts_table.php
+++ database/migrations/2025_03_14_033503_create_contacts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->TEXT('message');
             $table->timestamps();
         });
     }
